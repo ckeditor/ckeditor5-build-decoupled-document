a3902dd5dc6ffe2f7ad4aba5dff15e745c87e5f0
Nhien Nguyen <nhiennt@javisoft.vn>
2019-11-16 14:30:26 +0700

    add Base64UploadAdapter

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -35,6 +35,7 @@ import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
 import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice';
 import Table from '@ckeditor/ckeditor5-table/src/table';
 import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
+import Base64UploadAdapter from '@ckeditor/ckeditor5-upload/src/adapters/base64uploadadapter';
 
 export default class DecoupledEditor extends DecoupledEditorBase {}
 
@@ -68,7 +69,8 @@ DecoupledEditor.builtinPlugins = [
 	Paragraph,
 	PasteFromOffice,
 	Table,
-	TableToolbar
+	TableToolbar,
+	Base64UploadAdapter
 ];
 
 // Editor configuration.
